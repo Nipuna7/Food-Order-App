d073453a804191d53b538a17129d5ef7424901c4
Nipuna7 <nipunanirodha@gmail.com>
2025-03-04 15:52:28 +0530

    create loading screen

---
+++ foodapp/lib/screens/loading_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingScreen> {
+  double _opacity = 0.0; // Initial opacity value
+
+  @override
+  void initState() {
+    super.initState();
+    // Start the animation after the page is built
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() {
+        _opacity = 1.0; // Change opacity to 1.0 (fully visible)
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _opacity, // Use the animated opacity value
+          duration: Duration(seconds: 2), // Animation duration
+          child: Container(
+            height: 350, // Set height to 400px
+            width: 250, // Set width to 300px
+            child: Image.asset(
+              'assets/image/burger.jpg', // Replace with your image path
+              fit: BoxFit.cover, // Ensure the image covers the container
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
